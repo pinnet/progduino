f0f935a55eaa7eea4f5bec858d718e37da888cf1
Danny Arnold <danny@dannyarnold.com>
2019-04-17 20:55:15 +0100

    Audited Licence small refactor

--- Commands.ino
+++ Commands.ino
@@ -22,177 +22,6 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-void SetHardwarePins(){ 
-
-  //pinMode(chipSelect,  OUTPUT);
-  //digitalWrite(chipSelect, HIGH );
-
-  pinMode(ENCSW,  INPUT_PULLUP); 
-  pinMode(ENCA,   INPUT_PULLUP);
-  pinMode(ENCB,   INPUT_PULLUP);
-  pinMode(PIN_A0,  OUTPUT);
-  pinMode(PIN_A1,  OUTPUT);
-  pinMode(PIN_A2,  OUTPUT);
-  pinMode(PIN_A3,  OUTPUT);
-  pinMode(PIN_A4,  OUTPUT);
-  pinMode(PIN_A5,  OUTPUT);
-  pinMode(PIN_A6,  OUTPUT);
-  pinMode(PIN_A7,  OUTPUT);
-  pinMode(PIN_A8,  OUTPUT);
-  pinMode(PIN_A9,  OUTPUT);
-  pinMode(PIN_A10, OUTPUT);
-  pinMode(PIN_A11, OUTPUT);
-  pinMode(PIN_A12, OUTPUT);
-  pinMode(PIN_A13, OUTPUT);
-  pinMode(PIN_A14, OUTPUT);
-  pinMode(PIN_nCE, OUTPUT); digitalWrite(PIN_nCE, LOW); 
-  pinMode(PIN_nOE, OUTPUT); digitalWrite(PIN_nOE, HIGH);
-  pinMode(PIN_nWE, OUTPUT); digitalWrite(PIN_nWE, HIGH);
-  pinMode(PIN_HEARTBEAT, OUTPUT);
-  digitalWrite(PIN_HEARTBEAT, HIGH);
-  
-  pinMode(PIN_LED_RED, OUTPUT);
-  digitalWrite(PIN_LED_RED, LOW);
-  
-  pinMode(PIN_LED_GREEN, OUTPUT);
-  digitalWrite(PIN_LED_GREEN, LOW);
-
-  }
-  void SetDataLinesAsInputs()
-{
-  pinMode(PIN_D0, INPUT_PULLUP);
-  pinMode(PIN_D1, INPUT_PULLUP);
-  pinMode(PIN_D2, INPUT_PULLUP);
-  pinMode(PIN_D3, INPUT_PULLUP);
-  pinMode(PIN_D4, INPUT_PULLUP);
-  pinMode(PIN_D5, INPUT_PULLUP);
-  pinMode(PIN_D6, INPUT_PULLUP);
-  pinMode(PIN_D7, INPUT_PULLUP);
-}
-
-void SetDataLinesAsOutputs()
-{
-  pinMode(PIN_D0, OUTPUT);
-  pinMode(PIN_D1, OUTPUT);
-  pinMode(PIN_D2, OUTPUT);
-  pinMode(PIN_D3, OUTPUT);
-  pinMode(PIN_D4, OUTPUT);
-  pinMode(PIN_D5, OUTPUT);
-  pinMode(PIN_D6, OUTPUT);
-  pinMode(PIN_D7, OUTPUT);
-}
-
-void SetAddress(int a)
-{
-  digitalWrite(PIN_A0,  (a&1)?HIGH:LOW    );
-  digitalWrite(PIN_A1,  (a&2)?HIGH:LOW    );
-  digitalWrite(PIN_A2,  (a&4)?HIGH:LOW    );
-  digitalWrite(PIN_A3,  (a&8)?HIGH:LOW    );
-  digitalWrite(PIN_A4,  (a&16)?HIGH:LOW   );
-  digitalWrite(PIN_A5,  (a&32)?HIGH:LOW   );
-  digitalWrite(PIN_A6,  (a&64)?HIGH:LOW   );
-  digitalWrite(PIN_A7,  (a&128)?HIGH:LOW  );
-  digitalWrite(PIN_A8,  (a&256)?HIGH:LOW  );
-  digitalWrite(PIN_A9,  (a&512)?HIGH:LOW  );
-  digitalWrite(PIN_A10, (a&1024)?HIGH:LOW );
-  digitalWrite(PIN_A11, (a&2048)?HIGH:LOW );
-  digitalWrite(PIN_A12, (a&4096)?HIGH:LOW );
-  digitalWrite(PIN_A13, (a&8192)?HIGH:LOW );
-  digitalWrite(PIN_A14, (a&16384)?HIGH:LOW);
-}
-
-void SetData(byte b)
-{
-  digitalWrite(PIN_D0, (b&1)?HIGH:LOW  );
-  digitalWrite(PIN_D1, (b&2)?HIGH:LOW  );
-  digitalWrite(PIN_D2, (b&4)?HIGH:LOW  );
-  digitalWrite(PIN_D3, (b&8)?HIGH:LOW  );
-  digitalWrite(PIN_D4, (b&16)?HIGH:LOW );
-  digitalWrite(PIN_D5, (b&32)?HIGH:LOW );
-  digitalWrite(PIN_D6, (b&64)?HIGH:LOW );
-  digitalWrite(PIN_D7, (b&128)?HIGH:LOW);
-}
-
-byte ReadData()
-{
-  byte b = 0;
-
-  if (digitalRead(PIN_D0) == HIGH) b |= 1;
-  if (digitalRead(PIN_D1) == HIGH) b |= 2;
-  if (digitalRead(PIN_D2) == HIGH) b |= 4;
-  if (digitalRead(PIN_D3) == HIGH) b |= 8;
-  if (digitalRead(PIN_D4) == HIGH) b |= 16;
-  if (digitalRead(PIN_D5) == HIGH) b |= 32;
-  if (digitalRead(PIN_D6) == HIGH) b |= 64;
-  if (digitalRead(PIN_D7) == HIGH) b |= 128;
-
-  return(b);
-}
-
-// converts one character of a HEX value into its absolute value (nibble)
-byte HexToVal(byte b)
-{
-  if (b >= '0' && b <= '9') return(b - '0');
-  if (b >= 'A' && b <= 'F') return((b - 'A') + 10);
-  if (b >= 'a' && b <= 'f') return((b - 'a') + 10);
-  return(0);
-}
-String pad2center(String str,int size,String padding){
-  String pad = "";
-  int  n = str.length();
-  if (n >= size) return str;
-  
-  for (int a = 1; a <= (size - n )/ 2; a++){
-     pad += padding;
-  }
-  return pad + str + pad;
-}
-String padFromDividor(String str,int size,String padding,String dividor){
-  String pad = "";
-  int  n = str.length();
-  if (n >= size) return str;
-  
-  for (int a = 1; a <= (size - n )/ 2; a++){
-     pad += padding;
-  }
-  int cutline = str.indexOf(dividor);
-  return str.substring(0,cutline) + pad + pad + str.substring(cutline,str.length());
-}
-void SetTerm(){
-        term.init();
-        term.set_attribute(BT_BOLD);
-        term.cls();
-        term.position(0,0);
-        term.show_cursor(true);
-}
-void UnsetTerm(){
-  
-      term.set_attribute(BT_NORMAL);
-      term.set_color(BT_WHITE,BT_BLACK);
-      term.cls();
-      term.position(0,0);  
-}
-unsigned int hexUint(String hex){
-  
-  char crs[6];
-  hex.toCharArray(crs,6);
-  return strtol (crs,NULL,16);
-}
-
-
-void oledOut(String head, String msg){
-    display.setCursor(0, 0);
-    display.clearDisplay();
-    display.setTextSize((head.length() < 10)? 2 : 1);
-    display.setTextColor(WHITE);             
-    display.println(pad2center(head,(head.length() < 10)? 10 : 20," "));
-     
-    display.setTextSize(1); 
-    display.println();       
-    display.println(pad2center(msg,20," "));
-    display.display();
-    //display.startscrollright(0x00, 0x0F);
-}
 void changeMode(){
 
       if(cmd[1] != 0){ 
@@ -240,7 +69,6 @@ bool verifyPage(byte page){
     
       myFile.seek(addr + i);
       b = myFile.read();
-      //printinfo("Verify "+String(addr +i) +":"+String(b),String(i) +":"+ String(pageBuff[i]));
       if (pageBuff[i] != b){
         return false;
       }
@@ -307,6 +135,7 @@ void loadfile(){
     view = VIEW_DIRECTORY;
   }
   currentMode = MODE_FILE;
+  currentPage = 0;
   DisplayCurrentPage();  
 }
 
@@ -390,25 +219,6 @@ void erase(){
       
   }
 }
-void sanitycheck(){
-  sure =  true;
-  switch(doFunction){
-
-      case WRITE :
-        write();
-        break;
-      case ERASE :
-        erase();
-        break;
-      case ZERO :
-        zero();
-        break;
-      default :
-        break;
-  }
-
-
-}
 void write(){
 
   if(cmd[1]){ filename = cmd[1];} 
@@ -485,14 +295,3 @@ void FactoryReset(){
    delay(1500);
    ResetFunc();
 }
-bool areyousure(){
-
-  static char buf[2];
-      
-  if (readline(Serial.read(), buf, 2) > 0) {
-    if (buf[0] == 'y') {
-      return true;
-    }
-    else{  return false;   }
-    }
- }
--- View.ino
+++ View.ino
@@ -1,3 +1,28 @@
+/*
+MIT License
+
+Copyright (c) 2017 Danny Arnold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
 void setpage(){
     if (cmd[1].toInt() >= 0){
       currentPage = cmd[1].toInt(); 
@@ -261,4 +286,34 @@ void printinfo(String title,String message){
 
 
 
+}
+
+
+void oledOut(String head, String msg){
+    display.setCursor(0, 0);
+    display.clearDisplay();
+    display.setTextSize((head.length() < 10)? 2 : 1);
+    display.setTextColor(WHITE);             
+    display.println(pad2center(head,(head.length() < 10)? 10 : 20," "));
+     
+    display.setTextSize(1); 
+    display.println();       
+    display.println(pad2center(msg,20," "));
+    display.display();
+    //display.startscrollright(0x00, 0x0F);
+}
+
+void SetTerm(){
+        term.init();
+        term.set_attribute(BT_BOLD);
+        term.cls();
+        term.position(0,0);
+        term.show_cursor(true);
+}
+void UnsetTerm(){
+  
+      term.set_attribute(BT_NORMAL);
+      term.set_color(BT_WHITE,BT_BLACK);
+      term.cls();
+      term.position(0,0);  
 }
